1c8040cb49a1e17c7d7d3babedbfcbf7fdda4fad
Anton Lehmus <anton.lehmus@gmail.com>
2018-02-16 18:00:05 +0200

    clean up error handler

--- app.js
+++ app.js
@@ -50,10 +50,11 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send the error as json
   res.status(err.status || 500).json({
-    message: err.message,
-    error: err
+    error:{
+      message: err.message,
+    }
   });
 });
 
