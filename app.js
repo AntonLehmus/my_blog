e104e42f91b3657bdf393ad2eccbef5fff8411fd
Anton Lehmus <anton.lehmus@gmail.com>
2018-02-17 18:01:31 +0200

    add cors headers

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ const app = express();
 // Initialize knex.
 const knex = Knex(knexConfig[process.env.NODE_ENV]);
 
-// Bind all Models to a knex instance. If you only have one database in
+// Bind all Models to a knex instance
 Model.knex(knex);
 
-
+//midleware
 app.use(helmet());
 app.use(compression());
 app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
@@ -33,29 +33,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//allow cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Route registration
 app.use('/', index);
 app.use('/posts', posts);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // send the error as json
-  res.status(err.status || 500).json({
-    error:{
-      message: err.message,
-    }
-  });
+    // send the error as json
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message,
+        }
+    });
 });
 
 module.exports = app;
