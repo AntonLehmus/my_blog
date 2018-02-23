50daa3c52044aeb8f2e4b6fa407cdcc808a00cb8
Anton Lehmus <anton.lehmus@gmail.com>
2018-02-23 14:46:45 +0200

    add basic validation for paragraph routes

--- routes/paragraphs.js
+++ routes/paragraphs.js
@@ -1,26 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+const { buildSanitizeFunction } = require('express-validator/filter');
 
 const paragraphController = require('../controllers/paragraphController');
+const checkValidationResult = require('../middleware/checkValidationResult');
+
 
 
 /* GET all paragraphs. */
 router.get('/', paragraphController.get_all);
 
 /* GET paragraph by id. */
-router.get('/:id', paragraphController.find_by_id);
+router.get('/:id',
+    [ check('id').trim().isInt({min:1}),
+    sanitize('id').toInt() ],
+    checkValidationResult,
+    paragraphController.find_by_id);
 
 /* GET paragraph by post id. */
-router.get('/post/:id/', paragraphController.find_by_post_id);
+router.get('/post/:id/',
+    [ check('id').trim().isInt({min:1}),
+    sanitize('id').toInt() ],
+    checkValidationResult,
+    paragraphController.find_by_post_id);
 
 /* DELETE paragraph by id. */
-router.delete('/:id', paragraphController.delete_by_id);
+router.delete('/:id',
+    [ check('id').trim().isInt({min:1}),
+    sanitize('id').toInt() ],
+    checkValidationResult,
+    paragraphController.delete_by_id);
 
 /* CREATE new paragraph */
-router.post('/', paragraphController.create);
+router.post('/',
+    [check('header').exists(),
+    check('content').exists(),
+    check('post_id').exists().isInt({min:1}),
+    sanitize(['header','content'])],
+    checkValidationResult,
+    paragraphController.create);
 
 /* UPDATE paragraph by id */
-router.patch('/:id', paragraphController.patch);
+router.patch('/:id',
+    [ check('id').trim().isInt({min:1}),
+    sanitize('id').toInt(),
+    check('header').exists(),
+    check('content').exists(),
+    check('post_id').exists().isInt({min:1}),
+    sanitize(['header','content'])],
+    checkValidationResult,
+    paragraphController.patch);
 
 
 module.exports = router;
