702f86753894fd6c658929434064f9af9998fbfd
Anton Lehmus <anton.lehmus@gmail.com>
2018-04-07 18:06:56 +0300

    increaasre max pw and email length

--- db/migrations/20180301152250_create_users_table.js
+++ db/migrations/20180301152250_create_users_table.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('User', function(table){
             table.increments().primary();
-            table.string('email').notNullable().unique();
-            table.string('password').notNullable();
+            table.text('email').notNullable().unique();
+            table.text('password').notNullable();
             table.timestamps(false, true); //use dateTime as type instead of timestamp and default values to current timestamp
             table.index('email');
         })
